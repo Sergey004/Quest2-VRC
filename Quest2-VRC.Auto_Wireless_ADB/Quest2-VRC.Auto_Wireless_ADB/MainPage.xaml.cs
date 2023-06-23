290b5dfdaa886fc60fca11644838ddeb28339894
Sergey004 <sergey00475@gmail.com>
2023-06-23 20:22:27 +0300

    Addon for Quest2-VRC

---
+++ Quest2-VRC.Auto_Wireless_ADB/Quest2-VRC.Auto_Wireless_ADB/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Android;
+using Android.App;
+using Android.Content;
+using Android.Provider;
+using Plugin.LocalNotifications;
+using System;
+using Xamarin.Forms;
+[assembly: UsesPermission(Manifest.Permission.ReceiveBootCompleted)]
+namespace Quest2_VRC.Auto_Wireless_ADB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Clicked(object sender, EventArgs e)
+        {
+
+            Settings.Global.PutInt(Android.App.Application.Context.ContentResolver, "adb_wifi_enabled", (int)1);
+            int value = Settings.Global.GetInt(Android.App.Application.Context.ContentResolver, "adb_wifi_enabled");
+
+            DisplayAlert("Alert", $"adb_wifi_enabled is {value}", "OK");
+            CrossLocalNotifications.Current.Show("Wireless ADB", "Wireless ADB is now configured", 0, DateTime.Now.AddSeconds(1));
+        }
+        
+        [BroadcastReceiver(Name = "com.quest2_vrc.auto_wireless_adb.BootBroadcastReceiver", Enabled = true)]
+        [IntentFilter(new[] { Intent.ActionBootCompleted })]
+        public class BootBroadcastReceiver : BroadcastReceiver
+        {
+            public override void OnReceive(Context context, Intent intent)
+            {
+                Settings.Global.PutInt(Android.App.Application.Context.ContentResolver, "adb_wifi_enabled", (int)1);
+                CrossLocalNotifications.Current.Show("Wireless ADB", "Wireless ADB is now enabled", 0, DateTime.Now.AddSeconds(1));
+            }
+        }
+    }
+}
