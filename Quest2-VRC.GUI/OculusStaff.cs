046346146600cc671942a1f4e203eeadc74d9d57
Sergey004 <sergey00475@gmail.com>
2023-07-24 12:26:23 +0400

    Added command line function "--enhanced-oculus-control" for control Oculus services Like disable ASW and set High Priority for Oculus services

---
+++ Quest2-VRC.GUI/OculusStaff.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Quest2_VRC
+{
+    public class OculusStaff
+    {
+        public static async void DisableASW()
+        {
+            string OculusBase = "OculusBase";
+            var value = System.Environment.GetEnvironmentVariable(OculusBase);
+            Console.WriteLine(value + "Support\\oculus-diagnostics");
+            string loc = "-f " + AppDomain.CurrentDomain.BaseDirectory;
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = value + "Support\\oculus-diagnostics\\OculusDebugToolCLI.exe",
+                    Arguments = loc + "\\odtclicommands.txt",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            proc.Start();
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine();
+                Console.WriteLine(line);
+                File.AppendAllText("odtout.txt", line + Environment.NewLine);
+
+            }
+
+        }
+        public async static void HighPriority()
+        {
+
+            Process[] OculusClient = Process.GetProcessesByName("OculusClient");
+            foreach (Process process in OculusClient)
+            {
+                Console.WriteLine("Process name: {0}, ID: {1}", process.ProcessName, process.Id);
+                process.PriorityClass = ProcessPriorityClass.High;
+                Console.WriteLine(process.PriorityClass);
+            }
+            Process[] OculusDash = Process.GetProcessesByName("OculusDash");
+            foreach (Process process in OculusDash)
+            {
+                Console.WriteLine("Process name: {0}, ID: {1}", process.ProcessName, process.Id);
+                process.PriorityClass = ProcessPriorityClass.High;
+                Console.WriteLine(process.PriorityClass);
+            }
+            Process[] OVRServer_x64 = Process.GetProcessesByName("OVRServer_x64");
+            foreach (Process process in OVRServer_x64)
+            {
+                Console.WriteLine("Process name: {0}, ID: {1}", process.ProcessName, process.Id);
+                process.PriorityClass = ProcessPriorityClass.High;
+                Console.WriteLine(process.PriorityClass);
+            }
+            Console.WriteLine("Priority set to High");
+
+
+        }
+
+        public async static void NormalPriority()
+        {
+            Process[] OculusClient = Process.GetProcessesByName("OculusClient");
+            foreach (Process process in OculusClient)
+            {
+                Console.WriteLine("Process name: {0}, ID: {1}", process.ProcessName, process.Id);
+                process.PriorityClass = ProcessPriorityClass.Normal;
+                Console.WriteLine(process.PriorityClass);
+            }
+            Process[] OculusDash = Process.GetProcessesByName("OculusDash");
+            foreach (Process process in OculusDash)
+            {
+                Console.WriteLine("Process name: {0}, ID: {1}", process.ProcessName, process.Id);
+                process.PriorityClass = ProcessPriorityClass.Normal;
+                Console.WriteLine(process.PriorityClass);
+            }
+            Process[] OVRServer_x64 = Process.GetProcessesByName("OVRServer_x64");
+            foreach (Process process in OVRServer_x64)
+            {
+                Console.WriteLine("Process name: {0}, ID: {1}", process.ProcessName, process.Id);
+                process.PriorityClass = ProcessPriorityClass.Normal;
+                Console.WriteLine(process.PriorityClass);
+            }
+            Console.WriteLine("Priority set to Normal");
+
+        }
+    }
+}
--- Quest2-VRC.GUI/Program.cs
+++ Quest2-VRC.GUI/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Globalization;
+using System.Text;
 using System.Threading;
 using System.Windows.Forms;
+
 
 
 namespace Quest2_VRC
@@ -18,16 +20,22 @@ namespace Quest2_VRC
 
         static void Main(string[] args)
         {
+            
             foreach (string arg in args)
             {
 
                 switch (arg)
                 {
                     case "--help":
-                        Console.WriteLine("----Commands----\n--force-eng - Force enable English lang");
+                        Console.WriteLine("----Commands----\n--force-eng - Force enable English lang\n --enhanced-oculus-control - Enables enhanced management of Oculus services (Like disable ASW, sets High Priority for Oculus services");
                         break;
                     case "--force-eng":
                         Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+                        GUI();
+                        break;
+                    case "--enhanced-oculus-control":
+                        OculusStaff.DisableASW();
+                        OculusStaff.HighPriority();
                         GUI();
                         break;
                     default:
