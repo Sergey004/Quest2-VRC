897ecc0e32724e6baa5ae073f2ffd0739673b0fe
Sergey004 <sergey00475@gmail.com>
2024-01-01 16:24:31 +0400

    Add fallback for system without Oculus software

--- Quest2-VRC.Core/Services/Device_Management.cs
+++ Quest2-VRC.Core/Services/Device_Management.cs
@@ -9,12 +9,15 @@ namespace Quest2_VRC
     {
 
         private static readonly string DeviceName = "Oculus Composite ADB Interface";
+        private static readonly string FallbackName = "ADB Interface";
         [SupportedOSPlatform("windows")]
         public static bool CheckDevice()
         {
 
             bool deviceConnected = false;
             ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_PnPEntity where Caption='Oculus Composite ADB Interface'");
+            ManagementObjectSearcher fallback = new ManagementObjectSearcher("select * from Win32_PnPEntity where Caption='ADB Interface'");
+
             foreach (ManagementObject hmd in searcher.Get())
             {
                 foreach (PropertyData prop in hmd.Properties)
@@ -24,6 +27,20 @@ namespace Quest2_VRC
                         deviceConnected = true;
                         break;
                     }
+
+                }
+
+            }
+            foreach (ManagementObject hmd in fallback.Get())
+            {
+                foreach (PropertyData prop in hmd.Properties)
+                {
+                    if (Convert.ToString(prop.Value).Contains(FallbackName))
+                    {
+                        deviceConnected = true;
+                        break;
+                    }
+
                 }
 
             }
